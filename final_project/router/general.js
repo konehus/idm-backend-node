3bdb398134eec09ced5ae0ce2736f0ff76ac91bc
Henok Haile <konehus@gmail.com>
2023-07-16 21:32:32 +0000

    Improving the scope of Tasks 1-4 using Promises or Async-Await (#4)
    * Improving the scope of Tasks 1-4 using Promises or Async-Await
    
    - improve find book by isbn, author, title, and retrieve all books using promise callbacks
    
    * Fix typo

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,57 +22,101 @@ public_users.post("/register", (req, res) => {
   return res.status(404).json({ message: "Unable to register user." });
 });
 
+// Task 10: Add the code for getting the list of books available in the shop (done in Task 1) using Promise callbacks or async-await with Axios.
+function retrieveBooks() {
+  return new Promise((resolve, reject) => {
+    resolve(books);
+  });
+}
+
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
   //Write your code here
-  return res.send(JSON.stringify(books, null, 4));
+  retrieveBooks().then(
+    (books) => res.status(200).send(JSON.stringify(books, null, 4)),
+    (error) =>
+      res
+        .status(404)
+        .send("An error has occured trying to retrieve all the books")
+  );
 });
+
+// Task 11: Add the code for getting the book details based on ISBN (done in Task 2) using Promise callbacks or async-await with Axios.
+async function retrieveBookFromISBN(isbn) {
+  let book = books[isbn];
+  return new Promise((resolve, reject) => {
+    if (book) {
+      resolve(book);
+    } else {
+      reject(new Error("The provided book does not exist"));
+    }
+  });
+}
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  return res.send(books[isbn]);
+  retrieveBookFromISBN(isbn).then(
+    (book) => res.status(200).send(JSON.stringify(book, null, 4)),
+    (err) => res.status(404).send(err.message)
+  );
 });
+
+// Task 12: Retrieve book details by author using Promise Callbacks or async-await using axios
+async function retrieveBookFromAuthor(author) {
+  let validBooks = [];
+  return new Promise((resolve, reject) => {
+    for (let bookISBN in books) {
+      const bookAuthor = books[bookISBN].author;
+      if (bookAuthor === author) {
+        validBooks.push(books[bookISBN]);
+      }
+    }
+    if (validBooks.length > 0) {
+      resolve(validBooks);
+    } else {
+      reject(new Error("The provided author does not exist"));
+    }
+  });
+}
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
   //Write your code here
   const author = req.params.author;
-  let validBooks = [];
-  for (let bookISBN in books) {
-    const bookAuthor = books[bookISBN].author;
-    if (bookAuthor === author) {
-      validBooks.push(books[bookISBN]);
-    }
-  }
-
-  if (validBooks.length > 0) {
-    res.send(JSON.stringify(validBooks, null, 4));
-  } else {
-    return res.status(404).json({ message: "Provided author does not exist" });
-  }
+  retrieveBookFromAuthor(author).then(
+    (books) => res.status(200).send(JSON.stringify(books, null, 4)),
+    (err) => res.status(404).send(err.message)
+  );
 });
+
+// Task 13: Retrieve book details from title using Promise callbacks or async-await using axios
+function retrieveBookFromTitle(title) {
+  let validBooks = [];
+  return new Promise((resolve, reject) => {
+    for (let bookISBN in books) {
+      const bookTitle = books[bookISBN].title;
+      if (bookTitle === title) {
+        validBooks.push(books[bookISBN]);
+      }
+    }
+    if (validBooks.length > 0) {
+      resolve(validBooks);
+    } else {
+      reject(new Error("The provided book title does not exist"));
+    }
+  });
+}
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
   //Write your code here
   const title = req.params.title;
-  let validBooks = [];
-  for (let bookISBN in books) {
-    const bookTitle = books[bookISBN].title;
-    if (bookTitle === title) {
-      validBooks.push(books[bookISBN]);
-    }
-  }
-
-  if (validBooks.length > 0) {
-    res.send(JSON.stringify(validBooks, null, 4));
-  } else {
-    return res
-      .status(404)
-      .json({ message: "Provided book title does not exist" });
-  }
+  retrieveBookFromTitle(title).then(
+    (book) => res.status(200).send(JSON.stringify(book, null, 4)),
+    (err) => res.status(404).send(err.message)
+  );
 });
 
 //  Get book review
